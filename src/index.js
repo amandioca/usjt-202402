fe2a41555f5d61d5ab230d662d4c22c901088419
Amanda Amorim <amandaamorim.ctt@gmail.com>
2024-09-23 21:49:21 -0300

    feat: cria divisao responsiva de pedidos

--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => {
+    return (
+        <div>
+            <div className='container border mt-2'>
+                <div className='row'>
+                    <h1 className='text-center'>Seu pedido...</h1>
+                </div>
+                <div className='row mb-2'>
+                    <div className='col-xl-3 col-md-6  mb-2'>
+                        <div className="border p-5" align='center'>Pedido 1</div>
+                    </div>
+                    <div className='col-xl-3 col-md-6 mb-2'>
+                        <div className="border p-5" align='center'>Pedido 2</div>
+                    </div>
+                    <div className='col-xl-3 col-md-6 mb-2'>
+                        <div className="border p-5" align='center'>Pedido 3</div>
+                    </div>
+                    <div className='col-xl-3 col-md-6 mb-2'>
+                        <div className="border p-5" align='center'>Pedido 4</div>
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='col-xl-3 col-md-6 mb-2'>
+                        <div className="border p-5" align='center'>Pedido 5</div>
+                    </div>
+                    <div className='col-xl-3 col-md-6 mb-2'>
+                        <div className="border p-5" align='center'>Pedido 6</div>
+                    </div>
+                    <div className='col-xl-3 col-md-6 mb-2'>
+                        <div className="border p-5" align='center'>Pedido 7</div>
+                    </div>
+                    <div className='col-xl-3 col-md-6 mb-2'>
+                        <div className="border p-5" align='center'>Pedido 8</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+)
